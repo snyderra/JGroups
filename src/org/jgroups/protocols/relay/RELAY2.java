2e5dc9ba7f42d5dbda41e57dfd7b9b6c81c958c2
Bela Ban <belaban@mailbox.org>
2023-09-26 14:27:04 +0200

    - RELAY2 now also clears the headers and re-creates them from the buffer (same behavior as RELAY3). This should not pose any incompatibilities

--- src/org/jgroups/protocols/relay/RELAY2.java
+++ src/org/jgroups/protocols/relay/RELAY2.java
@@ -262,6 +262,15 @@ public class RELAY2 extends RELAY {
             log.warn("%s: received a message without a relay header; discarding it", local_addr);
             return;
         }
+        try {
+            msg.clearHeaders(); // remove all headers added by the bridge cluster
+            msg.putHeader(id, hdr);
+            ((BaseMessage)msg).readHeaders(hdr.headers());
+        }
+        catch(Exception ex) {
+            log.error("%s: failed handling message relayed from %s: %s", local_addr, msg.src(), ex);
+        }
+
         if(hdr.final_dest != null) {
             Message message=msg;
             RelayHeader header=hdr;
